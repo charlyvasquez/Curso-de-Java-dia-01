67be5f9c5400ce961cd8729709b3de211850303c
laoratorio <laoratorio@atl-labobd-311>
2015-06-15 15:00:13 -0430

    first commit

---
+++ JavaApplication1/src/javaapplication1/JavaApplication1.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author laoratorio
+ */
+public class JavaApplication1 {
+
+    private int contador;
+
+    public static void main(String[] args) {
+        JavaApplication1 jp1 = new JavaApplication1();
+        for (jp1.contador = 0; jp1.contador < 10; jp1.contador++) {
+            System.out.println("imprimiendo el contador \n \n" + jp1.contador);
+        }
+    }
+
+}
